7cd02b64ebfab93a027966f13141feea709b01f4
Takanori Yanagitani <yanagitani@adin.co.jp>
2020-06-04 16:00:46 +0900

    uuid(4x uint32) -> string

--- bytes2uuid.js
+++ bytes2uuid.js
@@ -8,14 +8,14 @@
 
   const nf = function(){ f(module.exports) }
   const bf = function(){
-    g.Bytes2UUID = {n}
+    g.Bytes2UUID = {}
     f(g.Bytes2UUID)
   }
 
   return m ? nf() : bf()
 })(this, function(e){
 
-const _byte2hex1 = function(){
+const byte2hex1 = function(){
   const b8map = Array.from({length:256})
     .map(function(_,i){ return 0x100 + i })
     .map(function(i){ return i.toString(16) })
@@ -23,18 +23,10 @@ const _byte2hex1 = function(){
   return function(b){ return b8map[b] }
 }
 
+const byte2hex = byte2hex1()
+
 const align_default = ~31
 const limit_default = 16777215
-
-const ab2dv = function(ab, offset, align, limit){
-  const o = offset || 0
-  const a = align  || align_default
-  const l = limit  || limit_default
-  const bl = ab.byteLength
-  const aligned = bl & align
-  const limited = aligned & limit
-  return new DataView(ab, o, limited)
-}
 
 const dv2u5 = function(dv, offset, littleEndian){
   const o = offset       || 0
@@ -51,8 +43,36 @@ const uuid_dv2u5 = function(dv, offset, littleEndian){
   return dv2u5(dv, 0, littleEndian)
 }
 
-e.array_buffer2dataview = ab2dv
+const u33s = function(a,b){ return byte2hex(a) + byte2hex(b) }
+const u3333s = function(a,b,c,d){ return byte2hex(a) + byte2hex(b) + byte2hex(c) + byte2hex(d) }
+
+const u4s = function(u4){ return u33s(u4 >>> 8, u4 & 0xff) }
+const u5s = function(u5){ return u3333s(
+  u5 >>> 24,
+  u5 >>> 16 & 0xff,
+  u5 >>>  8 & 0xff,
+  u5        & 0xff
+) }
+
+const stringify = function(lu5){ return u5s(lu5[0]) + u5s(lu5[1]) + u5s(lu5[2]) + u5s(lu5[3]) }
+
+const beautify = function(lu5){
+  const a  = lu5[0]
+  const bc = lu5[1]
+  const de = lu5[2]
+  const f  = lu5[3]
+  return [
+    u5s(a),
+    u4s(bc >>> 16), u4s(bc & 0xffff),
+    u4s(de >>> 16),
+    u4s(de & 0xffff) + u5s(f),
+  ].join("-")
+}
+
 e.data_view2uint32      = dv2u5
 e.from_dv               = uuid_dv2u5
+
+e.stringify = stringify
+e.beautify  = beautify
 
 })
--- bytes2uuid_test.js
+++ bytes2uuid_test.js
@@ -2,17 +2,58 @@ const bytes2uuid = require("./bytes2uuid")
 
 describe("node", () => {
 
-  describe("bytes2uuid.js", () => {
+describe("bytes2uuid.js", () => {
 
-    describe("uuid_from_dv", () => {
+  describe("uuid_from_dv", () => {
 
-      test("zero", () => Promise.resolve(new ArrayBuffer(16))
-        .then(ab => new DataView(ab))
-        .then(dv => bytes2uuid.from_dv(dv))
-        .then(uuid => expect(uuid).toStrictEqual([0,0,0,0]))
-      )
-    })
+    test("zero", () => Promise.resolve(new ArrayBuffer(16))
+      .then(ab => new DataView(ab))
+      .then(dv => bytes2uuid.from_dv(dv))
+      .then(uuid => expect(uuid).toStrictEqual([0,0,0,0]))
+    )
 
   })
+
+  describe("stringify", () => {
+    test("epoch", () => Promise.resolve(0x19700101)
+      .then(e => [e,e,e,e])
+      .then(bytes2uuid.stringify)
+      .then(s => expect(s).toBe("19700101197001011970010119700101"))
+    )
+  })
+
+  describe("beautify", () => {
+    test("epoch", () => Promise.resolve(0x19700101)
+      .then(e => [e,e,e,e])
+      .then(bytes2uuid.beautify)
+      .then(s => expect(s).toBe("19700101-1970-0101-1970-010119700101"))
+    )
+
+    test("f1", () => Promise.resolve(0x19700101)
+      .then(e => [0xf1234567,e,e,e])
+      .then(bytes2uuid.beautify)
+      .then(s => expect(s).toBe("f1234567-1970-0101-1970-010119700101"))
+    )
+
+    test("f2", () => Promise.resolve(0x19700101)
+      .then(e => [e,0xf1234567,e,e])
+      .then(bytes2uuid.beautify)
+      .then(s => expect(s).toBe("19700101-f123-4567-1970-010119700101"))
+    )
+
+    test("f3", () => Promise.resolve(0x19700101)
+      .then(e => [e,e,0xf1234567,e])
+      .then(bytes2uuid.beautify)
+      .then(s => expect(s).toBe("19700101-1970-0101-f123-456719700101"))
+    )
+
+    test("f4", () => Promise.resolve(0x19700101)
+      .then(e => [e,e,e,0xf1234567])
+      .then(bytes2uuid.beautify)
+      .then(s => expect(s).toBe("19700101-1970-0101-1970-0101f1234567"))
+    )
+  })
+
+})
 
 })
